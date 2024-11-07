fb3c1185fd37c0246e4f0036f2b647156fa70cd1
Kevin Asante <koasante@cps.edu>
2024-11-07 11:29:50 -0600

    fixed some

--- a4.py
+++ a4.py
@@ -9,9 +9,9 @@ class TTTBoard:
         board - a list of '*'s, 'X's & 'O's. 'X's represent moves by player 'X', 'O's
             represent moves by player 'O' and '*'s are spots no one has yet played on
     """
-    def __init__(self, X, O, ) -> None:
-        self.player = ['X']
-        self.player = ['O']
+    def __init__(self):
+        #self.player = [X]
+        #self.player = [O]
         self.board = ['*'] * 9
     
     def __str__(self) -> str:
@@ -50,7 +50,7 @@ class TTTBoard:
 def play_tic_tac_toe() -> None:
     """Uses your class to play TicTacToe"""
 
-    def is_int(maybe_int: str):
+    def __is_int__(maybe_int: str):
         """Returns True if val is int, False otherwise
 
         Args:
@@ -60,21 +60,20 @@ def play_tic_tac_toe() -> None:
             True if maybe_int is an int, False otherwise
         """
     
-
-        #if ValueError == int
-        #return False
-        
-        try:
-            int(maybe_int)
-            return True
-        except ValueError:
+    
+    try:
+        int(maybe_int)
+        return True
+    except ValueError:
             return False
+        
+    
 
-    brd = TTTBoard()
-    players = ["X", "O"]
-    turn = 0
+brd = TTTBoard()
+players = ["X", "O"]
+turn = 0
 
-    while not brd.game_over():
+while not brd.game_over():
         print(brd)
         move: str = input(f"Player {players[turn]} what is your move? ")
 
@@ -86,15 +85,15 @@ def play_tic_tac_toe() -> None:
         if brd.make_move(players[turn], int(move)):
             turn = not turn
 
-    print(f"\nGame over!\n\n{brd}")
+        print(f"\nGame over!\n\n{brd}")
     
-    if brd.has_won(players[0]):
+if brd.has_won(players[0]):
         print(f"{players[0]} wins!")
     
-    elif brd.has_won(players[1]):
+elif brd.has_won(players[1]):
         print(f"{players[1]} wins!")
     
-    else:
+else:
         print(f"Board full, cat's game!")
 
 
@@ -131,4 +130,4 @@ if __name__ == "__main__":
     print("All tests passed!")
 
     # uncomment to play!
-    # play_tic_tac_toe()
+    play_tic_tac_toe()

255726f193981a718c279d41f0626c9bb256d319
Kevin Asante <koasante@cps.edu>
2024-11-11 11:33:15 -0600

    writeup

--- a4.py
+++ a4.py
@@ -10,47 +10,51 @@ class TTTBoard:
             represent moves by player 'O' and '*'s are spots no one has yet played on
     """
     def __init__(self):
-        #self.player = [X]
-        #self.player = [O]
-        self.board = ['*'] * 9
-    
+        self.board = ['*']*9
+
     def __str__(self) -> str:
-        s = ''
-        for x in [0, 3, 6]:
-            s += self.board[x + 0] + "" + self.board[x + 1] + "" + self.board[x + 2]
-   
-    def make_move(self, player, pos) -> bool:
+        s= ""
+        for x in [0,3,6]:
+           s += self.board[x + 0] + " " + self.board[x+1] + " " + self.board[x+2] + "\n"
+        return s
+        
+    def make_move (self, player, pos) -> bool: 
         if pos < 0 or pos > 8 or self.board[pos] != '*':
             return False
+
         self.board[pos] = player
         return True
-    def has_won(self, player) -> bool:
-        ps = [player] * 3
-        
-        #Horizantal
-        if self.board[:3] == ps or self.board[3:6] == ps or self.board[6:] == ps:
+    
+    def has_won (self, player)-> bool:
+        """Check to see if the player has won"""
+        ps= [player]*3
+        # Horizontal      
+        if self.board[:3]==ps or self.board[3:6]==ps or self.board[6:]==ps:
+            return True
+        # Vertical
+        if self.board[::3]==ps or self.board[1::3]==ps or self.board[2::3]==ps:
+            return True
+        # Diagonal
+        if self.board[::4]==ps or self.board[2:7:2] == ps:
             return True
         
-        #Vertical
-        if self.board[::3] == ps or self.board[1::3] == ps or self.board[2::3] == ps:
-            return True
-        
-        #Diagonal
-        if self.board[::4] == ps or self.board[2:7:2] == ps:
-            return True
-   
-    def game_over(self) -> bool:
-        if self.has_won("X") or self.has_won("O") or "*" not in self.board:
-            return True
         return False
     
-    def clear(self) -> None:
-        self.board["*"] * 9
+    def game_over(self)->bool:
+        """Check if the board is full or if either X or O has won"""
+        if "*" not in self.board or self.has_won("X") or self.has_won("O"):
+            return True
+        
+        return False
+    
+    def clear(self)->None:
+        """Clear the board to reset game"""
+        self.board = ['*']*9
 
 def play_tic_tac_toe() -> None:
     """Uses your class to play TicTacToe"""
 
-    def __is_int__(maybe_int: str):
+    def is_int(maybe_int: str):
         """Returns True if val is int, False otherwise
 
         Args:
@@ -59,21 +63,19 @@ def play_tic_tac_toe() -> None:
         Returns:
             True if maybe_int is an int, False otherwise
         """
-    
-    
-    try:
-        int(maybe_int)
-        return True
-    except ValueError:
+        try:
+            int(maybe_int)
+            return True
+        except ValueError:
             return False
-        
-    
 
-brd = TTTBoard()
-players = ["X", "O"]
-turn = 0
+    brd = TTTBoard()
+    print(brd.board) 
+    print(brd)
+    players = ["X", "O"]
+    turn = 0
 
-while not brd.game_over():
+    while not brd.game_over():
         print(brd)
         move: str = input(f"Player {players[turn]} what is your move? ")
 
@@ -85,15 +87,12 @@ while not brd.game_over():
         if brd.make_move(players[turn], int(move)):
             turn = not turn
 
-        print(f"\nGame over!\n\n{brd}")
-    
-if brd.has_won(players[0]):
+    print(f"\nGame over!\n\n{brd}")
+    if brd.has_won(players[0]):
         print(f"{players[0]} wins!")
-    
-elif brd.has_won(players[1]):
+    elif brd.has_won(players[1]):
         print(f"{players[1]} wins!")
-    
-else:
+    else:
         print(f"Board full, cat's game!")
 
 
@@ -102,6 +101,8 @@ if __name__ == "__main__":
     # need to write some more tests to make sure that your TTTBoard class is behaving
     # properly.
     brd = TTTBoard()
+    print(brd.board)
+    print(brd)
     brd.make_move("X", 8)
     brd.make_move("O", 7)
 
@@ -131,3 +132,4 @@ if __name__ == "__main__":
 
     # uncomment to play!
     play_tic_tac_toe()
+    
